eee446bc9a9d234cb2bfe628c6acf87e0be51d5b
122515 <u84769093@cbi-bs1.server.lan>
2018-06-16 07:43:10 +0200

    add google analitycs tracjing code

--- wp-content/themes/tattoopro-child/functions.php
+++ wp-content/themes/tattoopro-child/functions.php
@@ -98,6 +98,25 @@ if ( ! function_exists( 'wolf_set_lang' ) ) {
 
 }
 
+//Google analitycs
+add_action('wp_head','my_analytics', 20);
+
+function my_analytics() {
+?>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-69336560-2', 'auto');
+ga('send', 'pageview');
+
+</script>
+
+<?php
+}
+
 // Masquer le message des erreurs de connexion
 add_filter('login_errors', '__return_null');
 // Maj auto
